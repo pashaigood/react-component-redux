1eb3173c62f8461c6de91cb46cb3fe33a2676108
Pavel Belugin <pashaigood@gmail.com>
2017-02-25 14:42:21 +0300

    fix(createStore): Removed immutable invariant middleware.
    Thunk set as deps.

--- local_modules/react-component-redux/helpers/configureStore.js
+++ local_modules/react-component-redux/helpers/configureStore.js
@@ -1,5 +1,4 @@
 import {createStore, compose, applyMiddleware} from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import commonMiddleware from '../middlewares/common';
@@ -26,9 +25,6 @@ function configureStoreDev(initialState) {
   const middlewares = [
     // Add other middleware on this line...
     commonMiddleware,
-
-    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-    reduxImmutableStateInvariant(),
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
